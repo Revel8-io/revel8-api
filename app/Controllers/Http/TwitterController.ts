187d4d4749f6e7638c644d1de08f7c0840b10688
Kylan Hurt <kylan.hurt@gmail.com>
2024-08-05 16:08:57 -0500

    Get Twitter user look up afer verification working

--- app/Controllers/Http/TwitterController.ts
+++ app/Controllers/Http/TwitterController.ts
@@ -23,9 +23,15 @@ export default class Twitter {
             reject(`Error in Twitter::getRequestToken: ${err}`)
           }
           console.log('tokenSecret, url', tokenSecret, url)
+          const searchParamString = url.split('?')[1]
+          const searchParams = new URLSearchParams(searchParamString)
+          const oauthToken = searchParams.get('oauth_token')
           // save tokenSecret to redis
-          Redis.set(`oauth_request:${rand}`, tokenSecret)
-          resolve({ tokenSecret, url })
+          Redis.set(`oauth_request:${oauthToken}`, tokenSecret, 'EX', 60 * 15)
+          resolve({
+            tokenSecret,
+            url
+          })
         })
     })
     }
@@ -34,7 +40,25 @@ export default class Twitter {
     return response.json(data)
   }
 
-  public async twitterCallback({}: HttpContextContract) {
-    console.log('in Twitter::twitterCallback')
+  public async twitterCallback({ view, request, response }: HttpContextContract) {
+    const { oauth_token, oauth_verifier } = request.qs()
+    console.log('in Twitter::twitterCallback', oauth_token, oauth_verifier)
+    const tokenSecret = await Redis.get(`oauth_request:${oauth_token}`)
+    console.log('tokenSecret', tokenSecret)
+    tw.callback({
+      oauth_token,
+      oauth_verifier
+    }, tokenSecret, (err, user) => {
+      if (err) {
+        console.error(`Error in Twitter::twitterCallback: ${err}`)
+        return response.json({
+          error: err
+        })
+      }
+      console.log('user', user)
+      return response.json(user)
+    })
+    console.log('returning view')
+    return view.render('twitter_callback')
   }
 }
