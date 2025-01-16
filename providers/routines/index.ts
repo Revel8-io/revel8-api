79755a5b17055779d84dd1dd0034dde731bcabf9
Kylan Hurt <kylan.hurt@gmail.com>
2025-01-15 23:32:37 -0300

    Get IPFS uploading working consistently

--- providers/routines/index.ts
+++ providers/routines/index.ts
@@ -4,7 +4,7 @@ import Env from '@ioc:Adonis/Core/Env'
 export const pinata = new PinataSDK({
   pinataJwt: Env.get('PINATA_JWT'),
   pinataGateway: Env.get('PINATA_GATEWAY'),
-  // add gateway key?
+  pinataGatewayKey: Env.get('PINATA_GATEWAY_KEY'),
 });
 
 export const testPinataAuth = async () => {
@@ -25,58 +25,121 @@ export const populateIPFSContent = async () => {
   // for each row, get the contents from the ipfs url and store it in atom_ipfs_data.contents
   // update atom_ipfs_data with the new contents
 
-  const rows = await Database.query().from('Atom')
+  const rows = await Database.from('Atom')
+    .select('Atom.*', 'atom_ipfs_data.*', 'Atom.id as atom_table_id')
     .fullOuterJoin('atom_ipfs_data', 'Atom.id', 'atom_ipfs_data.atom_id')
-    .where('Atom.data', 'like', 'ipfs://')
+    .where('Atom.data', 'like', 'ipfs://%')
     .orWhere('Atom.data', 'like', 'Qm%')
     .andWhereNull('atom_ipfs_data.contents')
 
   console.log('rows.length', rows.length)
 
-  // Semaphore implementation to limit concurrent operations
-  class Semaphore {
-    private running = 0;
-    private queue: (() => void)[] = [];
-
-    constructor(private maxConcurrent: number) {}
-
-    async acquire(): Promise<void> {
-      if (this.running < this.maxConcurrent) {
-        this.running++;
-        return;
+  const fetchIPFSContent = async (row: any) => {
+    return new Promise(async (resolve, reject) => {
+      let hash = ''
+      setTimeout(reject, 10000)
+      try {
+        setTimeout(reject, 10000)
+        hash = row.data.split('/').pop()
+        const contents = await pinata.gateways.get(hash)
+        resolve(contents)
+      } catch (_err) {
+        console.error('Error getting contents from IPFS', hash)
+        reject()
+      } finally {
+        resolve(void 0)
       }
+    })
+  }
 
-      return new Promise<void>(resolve => {
-        this.queue.push(resolve);
-      });
-    }
-
-    release(): void {
-      this.running--;
-      const next = this.queue.shift();
-      if (next) {
-        this.running++;
-        next();
-      }
+  for (const row of rows) {
+    console.log('row', row)
+    try {
+      const contents = await fetchIPFSContent(row)
+        // insert the contents into atom_ipfs_data
+        console.log('row.id', row.id)
+        const existingRows  = await Database.query().from('atom_ipfs_data').where('atom_id', row.atom_table_id)
+        console.log('existingRows.length', existingRows.length)
+        if (existingRows.length > 0) {
+          // update existing row
+          await Database.query()
+            .from('atom_ipfs_data')
+            .where('atom_id', row.atom_table_id)
+            .update({ contents });
+          console.log('finished updating')
+        } else {
+          await Database
+            .insertQuery()
+            .table('atom_ipfs_data')
+            .insert({ atom_id: row.atom_table_id, contents });
+          console.log('finished inserting')
+        }
+    } catch (err) {
+      console.error('Error getting contents from IPFS', err)
     }
   }
 
-  const semaphore = new Semaphore(5);
-  const promises = rows.map(async (row) => {
-    await semaphore.acquire();
-    try {
-      const hash = row.data.split('/').pop()
-      const contents = await pinata.gateways.get(hash)
-      // insert the contents into atom_ipfs_data
-      await Database.query()
-        .from('atom_ipfs_data')
-        .where('id', row.id)
-        .update({ contents });
-    } finally {
-      semaphore.release();
-    }
-  });
 
-  await Promise.all(promises);
-}
+
+  // Semaphore implementation to limit concurrent operations
+//   class Semaphore {
+//     private running = 0;
+//     private queue: (() => void)[] = [];
+
+//     constructor(private maxConcurrent: number) {}
+
+//     async acquire(): Promise<void> {
+//       if (this.running < this.maxConcurrent) {
+//         this.running++;
+//         return;
+//       }
+
+//       return new Promise<void>(resolve => {
+//         this.queue.push(resolve);
+//       });
+//     }
+
+//     release(): void {
+//       this.running--;
+//       const next = this.queue.shift();
+//       if (next) {
+//         this.running++;
+//         next();
+//       }
+//     }
+//   }
+
+//   const semaphore = new Semaphore(5);
+//   const promises = rows.map(async (row) => {
+//     await semaphore.acquire();
+//     let hash = ''
+//     try {
+//       hash = row.data.split('/').pop()
+//       const contents = await pinata.gateways.get(hash)
+//       // insert the contents into atom_ipfs_data
+//       const existingRows  = await Database.query().from('atom_ipfs_data').where('atom_id', row.id)
+//       console.log('existingRows.length', existingRows.length)
+//       if (existingRows.length > 0) {
+//         // update existing row
+//         await Database.query()
+//           .from('atom_ipfs_data')
+//           .where('atom_id', row.id)
+//           .update({ contents });
+//         console.log('finished updating')
+//       } else {
+//         await Database
+//           .insertQuery()
+//           .table('atom_ipfs_data')
+//           .insert({ atom_id: row.id, contents });
+//         console.log('finished inserting')
+//       }
+//     } catch (_err) {
+//       console.error('Error getting contents from IPFS', hash)
+//     } finally {
+//       semaphore.release();
+//     }
+//   });
+
+//   await Promise.all(promises);
+// }
 
