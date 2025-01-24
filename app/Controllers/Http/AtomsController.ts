c6a50f9b304c2ea4b8c4a9f4edd830e0bd211215
Kylan Hurt <kylan.hurt@gmail.com>
2025-01-23 23:49:48 -0300

    Get most relevant X atoms

--- app/Controllers/Http/AtomsController.ts
+++ app/Controllers/Http/AtomsController.ts
@@ -3,14 +3,17 @@ import Database from '@ioc:Adonis/Lucid/Database'
 
 import { CONFIG } from '../../../util/'
 
+const { IS_ATOM, IS_RELEVANT_X_ATOM } = CONFIG
+
 export default class AtomsController {
 
   public async getMostRelevantXAtoms({ response }: HttpContextContract) {
     const { rows } = await Database
       .rawQuery(`SELECT
-            "Triple".*,
-            "Vault".*,
-            "counterVault".*
+            "Triple"."subjectId", "Triple"."vaultId", "Triple"."counterVaultId",
+            "Vault"."totalShares" as "vaultTotalShares", "Vault"."currentSharePrice" as "vaultCurrentSharePrice", "Vault"."atomId" as "vaultAtomId", "Vault"."tripleId" as "vaultTripleId", "Vault"."positionCount" as "vaultPositionCount",
+            "counterVault"."totalShares" as "counterVaultTotalShares", "counterVault"."currentSharePrice" as "counterVaultCurrentSharePrice", "counterVault"."atomId" as "counterVaultAtomId", "counterVault"."tripleId" as "counterVaultTripleId", "counterVault"."positionCount" as "counterVaultPositionCount",
+            atom_ipfs_data.contents as "atomContents"
             FROM
                 "Triple"
             JOIN
@@ -21,17 +24,27 @@ export default class AtomsController {
               "Vault" AS "counterVault"
             ON
               "Triple"."counterVaultId" = "counterVault"."id"
-
+            LEFT JOIN "atom_ipfs_data"
+            ON "Triple"."subjectId" = "atom_ipfs_data"."atom_id"
             WHERE
-                "Triple"."objectId" = '${CONFIG.IS_RELEVANT_X_ATOM}'
-                AND "Triple"."predicateId" = '${CONFIG.IS_RELEVANT_X_TRIPLE}'
+                "Triple"."predicateId" = '${CONFIG.IS_ATOM}'
+                AND "Triple"."objectId" = '${CONFIG.IS_RELEVANT_X_ATOM}'
             ORDER BY
               GREATEST (
                 (("Vault"."totalShares"::NUMERIC / POWER(10, 18)) * ("Vault"."currentSharePrice"::NUMERIC) / POWER(10, 18)),
                 (("counterVault"."totalShares"::NUMERIC / POWER(10, 18)) * ("counterVault"."currentSharePrice"::NUMERIC) / POWER(10, 18))
               )
-            DESC;`)
+            DESC LIMIT 20;`)
     console.log('rows', rows)
+    // const relevantAtomIds = []
+    // rows.forEach((row) => {
+    //   relevantAtomIds.push(row.subjectId)
+    // })
+
+    // // console.log('relevantAtomIds', relevantAtomIds)
+    // const atomsData = await Database.query().from('atom_ipfs_data').whereIn('atom_id', relevantAtomIds)
+    // console.log('atomsData', atomsData)
     return response.json(rows)
   }
 }
+
--- util/constants.ts
+++ util/constants.ts
@@ -1,13 +1,13 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export const BASE_MAINNET_CONFIG = {
-  IS_RELEVANT_X_ATOM: 11359, // both incorrect!
-  IS_RELEVANT_X_TRIPLE: 45, // both incorrect!
+  IS_RELEVANT_X_ATOM: '11359', // both incorrect!
+  IS_ATOM: '45', // both incorrect!
 }
 
 export const BASE_SEPOLIA_CONFIG = {
-  IS_RELEVANT_X_ATOM: 11359,
-  IS_RELEVANT_X_TRIPLE: 45,
+  IS_RELEVANT_X_ATOM: '11359',
+  IS_ATOM: '45',
 }
 
 export const CONFIG = Env.get('NODE_ENV') === 'production' ? BASE_MAINNET_CONFIG : BASE_SEPOLIA_CONFIG
